b984fb6fa12af44c6e834884e8111297ed687bda
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2016-01-13 17:36:49 +0100

    Initial commit.

---
+++ SystemOutToolbox.Core/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace SystemOut.Toolbox
+{
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpper(this string input)
+        {
+            if (input == null)
+                throw new ArgumentException("Please provide a non null string.");
+            if (input == string.Empty)
+                return input;
+            return input.First().ToString().ToUpper() + input.Substring(1);
+        }
+    }
+}
