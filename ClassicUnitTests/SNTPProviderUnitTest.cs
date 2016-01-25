d3a1f261520261f006ad695e80091d39d4a8718b
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2016-01-25 16:48:27 +0100

    Implemented SNTP in classic .NET

---
+++ ClassicUnitTests/SNTPProviderUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SystenOut.Toolbox.Core.Classic;
+
+namespace ClassicUnitTests
+{
+    [TestClass]
+    public class SNTPProviderUnitTest
+    {
+        [TestMethod]
+        public void TestGetNetworkTime_AssertDayOfYearMatch()
+        {
+            Assert.AreEqual(DateTime.Today.DayOfYear, SNTPProvider.GetNetworkTime().DayOfYear);
+        }
+    }
+}
