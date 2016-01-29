7584876e8f60e6997fcb425e1e4ac83860a38cf4
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2016-01-30 00:00:39 +0100

    Added email validation.

---
+++ SystenOut.Toolbox.Core.Classic/RegexUtilities.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using static System.String;
+
+namespace SystenOut.Toolbox.Core.Classic
+{
+    public class RegexUtilities
+    {
+        bool invalid;
+
+        /// <summary>
+        /// Note that the IsValidEmail method does not perform authentication to validate the email address. It merely determines whether its format is valid for an email address.
+        /// https://msdn.microsoft.com/en-us/library/01escwtf(v=vs.110).aspx
+        /// </summary>
+        /// <param name="strIn">The string to validate</param>
+        /// <returns>True if valid email, otherwise false</returns>
+        public bool IsValidEmail(string strIn)
+        {
+            invalid = false;
+            if (IsNullOrEmpty(strIn))
+                return false;
+
+            // Use IdnMapping class to convert Unicode domain names.
+            try
+            {
+                strIn = Regex.Replace(strIn, @"(@)(.+)$", this.DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+
+            if (invalid)
+                return false;
+
+            // Return true if strIn is in valid e-mail format.
+            try
+            {
+                return Regex.IsMatch(strIn,
+                      @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                      @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                      RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+
+        private string DomainMapper(Match match)
+        {
+            // IdnMapping class with default property values.
+            var idn = new IdnMapping();
+
+            var domainName = match.Groups[2].Value;
+            try
+            {
+                domainName = idn.GetAscii(domainName);
+            }
+            catch (ArgumentException)
+            {
+                invalid = true;
+            }
+            return match.Groups[1].Value + domainName;
+        }
+    }
+}
